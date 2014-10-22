1e0abef2ec0264775068710e2202e6f8ed8644a0
fish <fish@localhost>
2014-10-22 14:36:40 +0800

    finish layout signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   get 'static_pages/home'
 
   get 'static_pages/help'
 
+  root to: 'static_pages#home'
+
+  match '/help', to: 'static_pages#help', via: 'get'
+  match '/signup', to: 'users#new', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
